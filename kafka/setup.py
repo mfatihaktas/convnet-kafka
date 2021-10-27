9530a4b793847c3fc3691e5cc558fc5d4e16222e
Mehmet Aktas <mfatihaktas@gmail.com>
2021-10-27 13:26:09 +0300

    Turned kafka into a lib. Made it possible to install all required libs, including convnetlib and kafkalib, at the root

---
+++ kafka/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='kafkalib',
+	version='0.1.0',
+	description='A Python library for writing/reading <topic, key, value> to/from Kafka.',
+	author='Mehmet Aktas',
+	author_email="mfatihaktas@gmail.com",
+	license='MIT',
+	install_requires=['confluent-kafka'],
+	packages=find_packages(include=['kafkalib']),
+	include_package_data=True)
---
+++ venv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VENV_DIR=./venv
+
+if [ $1 = 'setup' ]; then
+  python3 -m venv $VENV_DIR
+elif [ $1 = 'install' ]; then
+  pip3 install -r requirements.txt
+
+  python3 convnet/setup.py bdist_wheel
+  pip3 install --force-reinstall convnet/dist/convnetlib-0.1.0-py3-*.whl
+
+  python3 kafka/setup.py bdist_wheel
+  pip3 install --force-reinstall kafka/dist/kafkalib-0.1.0-py3-*.whl
+elif [ $1 = 'source' ]; then
+  source $VENV_DIR/bin/activate
+else
+  echo "Arg did not match; arg= "$1
+fi
